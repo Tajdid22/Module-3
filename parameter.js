9a489cf7bc788c3593cf565782fea65b02ba1b2d
Tajdid22 <tajdidmahmud63@gmail.com>
2025-12-04 16:00:06 +0600

    Final one

---
+++ parameter.js
@@ -0,0 +1,13 @@
+//single
+const first = (x) => x
+console.log(first(x="Hello Tajdid Bhaiya"))
+
+
+//Multiple
+const second = (x,y,z) => x+y+z
+console.log(second(x=10, y=10, z=10))
+
+
+// No parameter
+const third = () => "Hello Jarin Apu"
+console.log(third())
---
+++ scope.js
@@ -0,0 +1,17 @@
+//Global
+
+let lang = ("English")
+
+const show = () => {
+    console.log(lang);
+};
+show ();
+
+//Local
+
+
+const show = () => {
+    let lang = ("English")
+    console.log(lang);
+};
+show ();
